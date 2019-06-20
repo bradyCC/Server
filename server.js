9d91315e7f0b63c62499c7b6d138513340ebb788
brady <acheng_69@163.com>
2019-06-20 17:22:51 +0800

    Express 配置Server服务

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  res.send({path: 'Home'});
+});
+
+app.get('/about', (req, res) => {
+  res.send({path: 'About Us'});
+})
+
+app.get('/products', (req, res) => {
   res.send([
-    { user: 'Lucy' },
-    { data: [] },
-  ]);
+    {id: 1, title: 'Product A'},
+    {id: 2, title: 'Product B'},
+    {id: 3, title: 'Product C'},
+  ])
 });
 
 app.listen(3000, () => {
