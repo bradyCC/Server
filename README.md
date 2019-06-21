4b17fe57fd426cf89014af330378b30af9317dc4
brady <acheng_69@163.com>
2019-06-21 09:30:01 +0800

    获取产品列表、产品详情接口完成

--- README.md
+++ README.md
@@ -21,22 +21,29 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/express', {useNewUrlParser: true})
 ```
 
-## 三、创建模型
+## 三、创建Model模型
 > 创建模型
 ```javascript
 const Product = mongoose.model('Product', new mongoose.Schema({title: String,}), 'products')
 ```
 
 ## 四、基本使用
-```javascript
+```
 // 查询
-Product.find()
+Product.find() // 全部查询
+Product.find().skip(1).limit(2) // 跳过、限制查询 - 多用于分页
+Product.find().where({title: '产品2'}) // 条件查询
+Product.find().sort({_id: 1}) // 排序 1-正序 2-倒序
+Product.findOne({_id: req.params.id}) // URL传参查询
+Product.findById(req.params.id) // ID查询
+
 
 // 全部删除
 Product.deleteMany({})
 
 // 批量插入
 Product.insertMany([])
+
 
 ```
 
--- server.js
+++ server.js
@@ -5,19 +5,12 @@ const express = require('express')
 
 const app = express()
 
-// 操作mongodb数据库
+// 连接mongodb数据库
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/express', {useNewUrlParser: true})
-// 创建模型
-const Product = mongoose.model('Product', new mongoose.Schema({
-  title: String,
-}), 'products')
 
-// Product.insertMany([
-//   {title: '产品1'},
-//   {title: '产品2'},
-//   {title: '产品3'},
-// ])
+// 创建model模型
+const Product = mongoose.model('Product', new mongoose.Schema({title: String}), 'products')
 
 const cors = require('cors')
 // 处理跨域
@@ -26,19 +19,31 @@ app.use(cors())
 // 静态文件托管
 app.use('/static', express.static('public'))
 
+//
 app.get('/', (req, res) => {
-  res.send({path: 'Home'});
+  res.send({path: 'Home'})
 })
 
+//
 app.get('/about', (req, res) => {
-  res.send({path: 'About Us'});
+  res.send({path: 'About Us'})
 })
 
+// 获取产品列表
 app.get('/products', async (req, res) => {
   const data = await Product.find()
   res.send(data)
-});
+})
 
+// 获取产品详情
+app.get('/products/:id', async (req, res) => {
+  const data = await Product.findById(req.params.id)
+  res.send(data)
+})
+
+
+
+//
 app.listen(3000, () => {
   console.log('App listening on port 3000')
 })
